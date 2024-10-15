700f70562a78c70526abfab91595bcd1a79a42a3
Pedro Corsi <pedrocorsi1@outlook.com>
2024-10-15 14:11:09 -0300

    fix: edited sequelize

--- backend/src/config/sequelize-config.ts
+++ backend/src/config/sequelize-config.ts
@@ -3,19 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// export const sequelize = new Sequelize({
-//   database: process.env.DB_NAME,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   host: process.env.DB_HOST,
-//   port: Number(process.env.DB_PORT),
-//   dialect: 'postgres',
-//   dialectOptions: {
-//     ssl: process.env.INTERNAL_DB_URL
-//   }
-// });
-
-export const sequelize = new Sequelize(process.env.INTERNAL_DB_URL as string, {
+export const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
@@ -24,3 +17,13 @@ export const sequelize = new Sequelize(process.env.INTERNAL_DB_URL as string, {
     },
   },
 });
+
+// export const sequelize = new Sequelize(process.env.INTERNAL_DB_URL as string, {
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,  // Allow self-signed SSL certificates if needed
+//     },
+//   },
+// });
