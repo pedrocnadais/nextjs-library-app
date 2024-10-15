dfeb7464cef813538ce7191be215d8b40c45ac49
Pedro Corsi <pedrocorsi1@outlook.com>
2024-10-15 16:41:57 -0300

    fix: changed sequelize

--- backend/src/config/sequelize-config.ts
+++ backend/src/config/sequelize-config.ts
@@ -3,32 +3,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const dbUrl = process.env.INTERNAL_DB_URL || process.env.EXTERNAL_DB_URL
+// const dbUrl = process.env.INTERNAL_DB_URL || process.env.EXTERNAL_DB_URL
 
-export const sequelize = new Sequelize(dbUrl as string, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,  // Allow self-signed SSL certificates if needed
-    },
-  },
-});
-
-// export const sequelize = new Sequelize({
-//   database: process.env.DB_NAME,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   host: process.env.DB_HOST,
-//   port: Number(process.env.DB_PORT),
+// export const sequelize = new Sequelize(dbUrl as string, {
 //   dialect: 'postgres',
-//   // dialectOptions: {
-//   //   ssl: process.env.INTERNAL_DB_URL
-//   //     // ? {
-//   //     //     // require: true,
-//   //     //     rejectUnauthorized: false,
-//   //     //   }
-//   //     // : false, // Disable SSL if no connection string
-//   // },
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,  // Allow self-signed SSL certificates if needed
+//     },
+//   },
 // });
+
+export const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  dialect: 'postgres',
+});
 
