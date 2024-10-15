d3dbaac40f4278bf74d00d3b20ab01cc75593cfb
Pedro Corsi <pedrocorsi1@outlook.com>
2024-10-15 13:35:27 -0300

    fix: changed sequelize

--- backend/src/config/sequelize-config.ts
+++ backend/src/config/sequelize-config.ts
@@ -3,14 +3,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+// export const sequelize = new Sequelize({
+//   database: process.env.DB_NAME,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT),
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: process.env.INTERNAL_DB_URL
+//   }
+// });
+
+export const sequelize = new Sequelize(process.env.INTERNAL_DB_URL as string, {
   dialect: 'postgres',
   dialectOptions: {
-    ssl: process.env.INTERNAL_DB_URL
-  }
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,  // Allow self-signed SSL certificates if needed
+    },
+  },
 });
