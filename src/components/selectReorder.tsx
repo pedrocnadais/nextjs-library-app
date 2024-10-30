fd07409d19f9500f895c10eb2b7a49010c9a6a86
Pedro Corsi <pedrocorsi1@outlook.com>
2024-10-30 15:24:27 -0300

    fix: added label tag to selectReorder

--- src/components/selectReorder.tsx
+++ src/components/selectReorder.tsx
@@ -9,7 +9,8 @@ interface SelectReorderProps {
 
 const SelectReorder: React.FC<SelectReorderProps> = ({ setSortBy, sortBy }) => {
   return (
-    <>
+    <label>
+      Sort books by:
       <select
         className="border rounded-lg bg-[#fdfdfd] p-1 shadow-sm"
         value={sortBy}
@@ -37,7 +38,7 @@ const SelectReorder: React.FC<SelectReorderProps> = ({ setSortBy, sortBy }) => {
           Title (z-a)
         </option>
       </select>
-    </>
+    </label>
   );
 };
 
